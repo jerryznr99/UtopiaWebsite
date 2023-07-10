009098d0c2bb732dc676f88016b15b8c8aee8a5a
jerryznr99 <99460248+jerryznr99@users.noreply.github.com>
2023-07-09 21:40:24 -0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
+"use client"
+
+import React from 'react';
+import {useEffect} from 'react';
 import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+let address = "0xa70b638B70154EdfCbb8DbbBd04900F328F32c35";
+
+const App = () => {
+
+    const handleFetchData = async () => {
+        // const response = await fetch("http://localhost:3000/api/example");
+        const response = await fetch("http://localhost:3000/api/example?" + new URLSearchParams({input: address}));
+        console.log(response)
+        const data = await response.json();
+        console.log(data);
+    }
+    
+    function getAddress(){
+        address = document.getElementById('address').value;
+        handleFetchData();
+    }
+    // useEffect(() => {
+    //     handleFetchData();        
+    // },[])
+
+    return(
+        <div className="App">
+
+            <div className="Navigation w-[1440px] h-[66px] p-4 bg-white border border-zinc-200 justify-start items-start gap-4 inline-flex">
+                <div className="Left grow shrink basis-0 h-9 justify-start items-start gap-4 flex">
+                    <div className="ElementLogo py-0.5 flex-col justify-start items-start gap-2 inline-flex" />
+                    <Image 
+                        src = "/Vector.svg"
+                        width = {44} 
+                        height = {32}
+                    />
+                </div>
+            </div>
+
+            <div className="EmptyState w-96 h-72 px-96 flex-col justify-start items-center gap-6 inline-flex">
+                <img className="Cloud2x w-48 h-24" src="/Cloud@2x.png" />
+                <div className="Content h-28 flex-col justify-center items-center gap-2 flex">
+                    <div className="Prompt self-stretch text-center text-zinc-700 text-3xl font-semibold leading-loose">How much money have you spent on gas?</div>
+                    <div className="InsertSafeAddress self-stretch text-center text-zinc-500 text-base font-normal leading-normal">Insert your safe address below to check how much money you’ve spent on gas.</div>
+                </div>
+                <div className="Row w-96 justify-start items-start gap-2 inline-flex">
+                    <div className="FieldTagSelect grow shrink basis-0 rounded flex-col justify-start items-start inline-flex">
+                        <div className="FormFieldAction self-stretch rounded border border-zinc-200 justify-start items-start inline-flex">
+                            <input 
+                                className="SelectOption"
+                                placeholder="Safe Address"
+                                id='address'
+                            />
+                        </div>
+                    </div>
+                    <button onClick={getAddress} className="Button">Calculate</button>
+                </div>
+            </div>
+
+            <div className="Card">
+                <div className="Text">
+                    <div className="Frame427319277 self-stretch justify-start items-start gap-1 inline-flex">
+                        <Image 
+                            src = "/local_gas_station.svg"
+                            width = {18} 
+                            height = {18}
+                        />
+                        <div className="TotalGasSpentByAllOfUtopia grow shrink basis-0 text-slate-500 text-xs font-semibold leading-none">TOTAL GAS SPENT BY ALL OF UTOPIA</div>
+                    </div>
+                    <div className="Frame427319278 px-6 flex-col justify-start items-start gap-2.5 flex">
+                        <div className="Frame11 justify-end items-end gap-1 inline-flex">
+                            <div className="Frame12 justify-start items-start gap-0.5 flex">
+                                <div className="00 text-zinc-700 text-sm font-normal leading-tight">0.00</div>
+                                <div className="Token justify-start items-start flex">
+                                    <div className="Fiat w-5 h-5 relative">
+                                        <Image 
+                                            src = "/Fiat.svg"
+                                            width = {20} 
+                                            height = {20}
+                                        />
+                                    </div>
+                                    <div className="Usd text-zinc-700 text-sm font-normal leading-tight">USD</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* <h1>How much gas can you save using Utopia?</h1>
+
+            <div className = "search">
+                <input 
+                    placeholder = 'Enter your address'
+                    id = 'address'
+                />
+            </div><br></br>
+        
+            <button onClick={getAddress}>Submit</button> */}
+
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
+
+export default App;
